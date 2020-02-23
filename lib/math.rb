43d78c515cef07c70c28cfea89c6017f7ade88a2
Ras azzab <ras.dinho7@gmail.com>
2020-02-23 04:17:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+adds=num2+num1
 end
 
 def subtraction(num1, num2)
-
+subtract=num2+num1
 end
 
 def division(num1, num2)
-
+divide=num2/num1
 end
 
 def multiplication(num1, num2)
-
+mult=num1*num2
 end
 
 def modulo(num1, num2)
-
+modu=num2%num1
 end
 
 def square_root(num)
-
+root=num.square_root
 end
